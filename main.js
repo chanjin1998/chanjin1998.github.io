7ef5e06903f7f666351b7ffe646f39417798407b
chanjin1998 <chanjin@kookmin.ac.kr>
2023-03-20 01:55:10 +0900

    docs: 순서변경

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-function showDiv() {
-	$('#noneDiv').show(); /* body-content를 보이게 합니다. */
+function showPhoto() {
+	$('#photo').show(); /* body-content를 보이게 합니다. */
   }
-function hideDiv(){
-	$('#noneDiv').hide();
+function hidePhoto(){
+	$('#photo').hide();
+}
+
+function showMemo() {
+	$('#memo').show(); /* body-content를 보이게 합니다. */
+  }
+function hideMemo(){
+	$('#memo').hide();
+}
+
+function showMusic() {
+	$('#music').show(); /* body-content를 보이게 합니다. */
+  }
+function hideMusic(){
+	$('#music').hide();
+}
+
+function showMap() {
+	$('#map').show(); /* body-content를 보이게 합니다. */
+}
+function hideMap(){
+	$('#map').hide();
+}
+
+function showMovie() {
+	$('#movie').show(); /* body-content를 보이게 합니다. */
+}
+function hideMovie(){
+	$('#movie').hide();
 }
   
