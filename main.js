d7b6811ffd2f1da9309c2e91e0711e04bcd99795
chanjin1998 <chanjin@kookmin.ac.kr>
2023-03-20 16:06:43 +0900

    docs: prettier

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
 function showPhoto() {
-	$('#photo').show(); /* body-content를 보이게 합니다. */
-  }
-function hidePhoto(){
-	$('#photo').hide();
+  $("#photo").show(); /* body-content를 보이게 합니다. */
+}
+function hidePhoto() {
+  $("#photo").hide();
 }
 
 function showMemo() {
-	$('#memo').show(); /* body-content를 보이게 합니다. */
-  }
-function hideMemo(){
-	$('#memo').hide();
+  $("#memo").show(); /* body-content를 보이게 합니다. */
+}
+function hideMemo() {
+  $("#memo").hide();
 }
 
 function showMusic() {
-	$('#music').show(); /* body-content를 보이게 합니다. */
-  }
-function hideMusic(){
-	$('#music').hide();
+  $("#music").show(); /* body-content를 보이게 합니다. */
+}
+function hideMusic() {
+  $("#music").hide();
 }
 
 function showCamera() {
-	$('#camera').show(); /* body-content를 보이게 합니다. */
+  $("#camera").show(); /* body-content를 보이게 합니다. */
 }
-function hideCamera(){
-	$('#camera').hide();
+function hideCamera() {
+  $("#camera").hide();
 }
 
 function showMovie() {
-	$('#movie').show(); /* body-content를 보이게 합니다. */
+  $("#movie").show(); /* body-content를 보이게 합니다. */
 }
-function hideMovie(){
-	$('#movie').hide();
+function hideMovie() {
+  $("#movie").hide();
 }
-  
